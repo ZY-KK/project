fc18638544cefb0546df2595306021358d8d1cea
Yi Zhou <yii.zhou@outlook.com>
2021-06-12 13:28:10 +0300

    add callback

--- Environment/main.py
+++ Environment/main.py
@@ -1,4 +1,3 @@
-
 from stable_baselines3.ppo.ppo import PPO
 import torch.nn as nn
 import gym
@@ -16,6 +15,7 @@ from stable_baselines3.common.env_checker import check_env
 import numpy as np
 from custom_policy import CustomCNN, CustomActorCriticPolicy
 from stable_baselines3.common.evaluation import evaluate_policy
+from callback import CheckpointCallback, EvalCallback, SaveVecNormalizeCallback
 env = PandaGraspEnv(sim = PyBullet(render =True))
 '''
 env = ProcessFrame84(env)
@@ -23,4 +23,23 @@ env = ProcessFrame84(env)
 '''
 env = ProcessDepthFrame84(env)
 env = DepthToPyTorch(env)
-
+env.reset()
+policy_kwargs = dict(
+    features_extractor_class=CustomCNN,
+    features_extractor_kwargs=dict(features_dim=128),
+)
+checkpoint_callback = CheckpointCallback(save_freq=1000, save_path='./log/',
+                                         name_prefix='rl_model')
+save_vec_normalize = SaveVecNormalizeCallback(
+                save_freq=1, save_path='./log/Vec/')
+eval_callback = EvalCallback(
+                eval_env=env,
+                callback_on_new_best=save_vec_normalize,
+                best_model_save_path='./log/best_model/',
+                n_eval_episodes=5,
+                log_path='./log/Eval/',
+                eval_freq=10000,
+                deterministic=True,
+            )
+model = PPO("CnnPolicy",env=env, policy_kwargs=policy_kwargs, verbose=1, tensorboard_log='/tmp/ppo/')
+model.learn(1000)
